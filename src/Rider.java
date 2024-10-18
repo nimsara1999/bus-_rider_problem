d369b54133e5905ec57d9ccaf7a8b11f6b18b042
sasindu-wijesuriya <sasindu.20@cse.mrt.ac.lk>
2024-10-18 22:54:38 +0530

    Refactor Rider class to use a Queue for rider waiting and update print statements

--- src/Rider.java
+++ src/Rider.java
@@ -1,35 +1,37 @@
 import java.util.concurrent.Semaphore;
 import java.util.Random;
+import java.util.Queue;
 
 public class Rider extends Thread {
     private Semaphore mutex;
-    private Semaphore bus;
-    private Semaphore boarded;
-    private int[] waiting;
+    private Semaphore bus_semaphore;
+    private Semaphore boarded_semaphore;
+    private Queue<Rider> riderQueue;
     private int index;
     Random random = new Random();
 
-    public Rider(Semaphore mutex, Semaphore bus, Semaphore boarded, int[] waiting, int index) {
+    public Rider(Semaphore mutex, Semaphore bus_semaphore, Semaphore boarded_semaphore, Queue<Rider> riderQueue,
+            int index) {
         this.mutex = mutex;
-        this.bus = bus;
-        this.boarded = boarded;
-        this.waiting = waiting;
+        this.bus_semaphore = bus_semaphore;
+        this.boarded_semaphore = boarded_semaphore;
+        this.riderQueue = riderQueue;
         this.index = index;
     }
 
     @Override
     public void run() {
         try {
-            // Acquire the mutex to increment the number of waiting riders
+            // Acquire the mutex to safely add the rider to the queue
             mutex.acquire();
-            waiting[0]++;
-            System.out.println("Rider arrives. Riders waiting: " + waiting[0]);
+            riderQueue.add(this);
+            System.out.println("Rider " + (index + 1) + " arrives. Waiting...");
             mutex.release();
 
             // Wait for a bus to signal that it is ready for boarding
-            bus.acquire();
+            bus_semaphore.acquire();
             board(random);
-            boarded.release(); // Signal that the rider has boarded the bus
+            boarded_semaphore.release(); // Signal that the rider has boarded the bus
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -39,6 +41,11 @@ public class Rider extends Thread {
     private void board(Random random) throws InterruptedException {
         int randomTime = 800 + random.nextInt(401);
         Thread.sleep(randomTime);
-        System.out.println(index + 1 + " Rider boards the bus.");
+        System.out.println("Rider " + (index + 1) + " boards the bus.");
+    }
+
+    @Override
+    public String toString() {
+        return (index + 1) + " ";
     }
 }
